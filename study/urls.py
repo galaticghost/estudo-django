8f5aee7e3d23f0fe0d261db1b04f93a32a9e2c1e
Arthur Atitus <1137370@atitus.edu.br>
2025-04-15 20:50:51 -0300

    Views e templates

--- study/urls.py
+++ study/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bola import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('bola/',views.index,name='bola'),
 ]
